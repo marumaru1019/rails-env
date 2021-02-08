25577013de0c8ba736b82fecd5010af6749393ce
Ryusei OYA <ryuryusei401@toki.waseda.jp>
2021-02-09 03:11:15 +0900

    vueの追加

---
+++ vue-pack.sh
@@ -0,0 +1,7 @@
+#　webコンテナ内で使用する
+
+yarn add axios
+yarn add vue-router
+yarn add vue-template-compiler
+yarn add vuex
+yarn add vue-eslint-parser
---
+++ vue-start.sh
@@ -0,0 +1,9 @@
+
+#
+docker-compose run web yarn install --check-files
+
+# webpackerのインストール
+docker-compose run web bin/rails webpacker:install
+
+# vueのインストール
+docker-compose run web bin/rails webpacker:install:vue
