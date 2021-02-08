7923e51a551a125cca9a6006c7229ccb925bca9a
Ryusei OYA <ryuryusei401@toki.waseda.jp>
2021-02-09 02:56:49 +0900

    no message

---
+++ add-pack.sh
@@ -0,0 +1,11 @@
+# dockerコンテナ内でインストールする
+# docker-compose exec app bash
+
+#ローカルでした場合以下のコマンドが必要になる
+# docker-compose run web yarn install --check-files
+
+yarn add axios
+yarn add vue-router
+yarn add vue-template-compiler
+yarn add vuex
+yarn add vue-eslint-parser
